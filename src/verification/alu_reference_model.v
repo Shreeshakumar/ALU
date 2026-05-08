1b892f7457f75a047f672e969347e8bbbeba3a63
shreeshakumar <shreeshamatandoor@gmail.com>
2026-05-08 11:04:03 +0530

    updated reference model basic level

--- src/verification/alu_reference_model.v
+++ src/verification/alu_reference_model.v
@@ -1,12 +1,10 @@
-//==============================================
-// ALU Reference Model
-//==============================================
-
 module alu_reference_model(
+	input CLK, RST,
+	input [1:0]INP_VALID,
     input [7:0] OPA, OPB,
     input CIN, MODE,
     input [3:0] CMD,
-    output reg [8:0] RES,
+    output reg [16:0] RES,
     output reg COUT, OFLOW, G, E, L, ERR
 );
 
@@ -14,27 +12,38 @@ module alu_reference_model(
 
     always @(*) begin
         // Default values
-        RES = 9'bz;
-        COUT = 1'bz;
-        OFLOW = 1'bz;
-        G = 1'bz;
-        E = 1'bz;
-        L = 1'bz;
-        ERR = 1'bz;
+        RES = 9'b0;
+        COUT = 1'b0;
+        OFLOW = 1'b0;
+        G = 1'b0;
+        E = 1'b0;
+        L = 1'b0;
+        ERR = 1'b0;
 
+		if (RST)
+		begin
+        	RES = 9'b0;
+        	COUT = 1'b0;
+        	OFLOW = 1'b0;
+        	G = 1'b0;
+        	E = 1'b0;
+        	L = 1'b0;
+        	ERR = 1'b0;
+		end
+		else
+		if (CE)
+		begin
         if (MODE) begin  // Arithmetic Mode
             case(CMD)
                 4'b0000: begin  // ADD
-                    RES = OPA + OPB;
-                    COUT = RES[8];
+                    {COUT,RES[7:0]} = OPA + OPB;
                 end
                 4'b0001: begin  // SUB
                     OFLOW = (OPA < OPB);
                     RES = OPA - OPB;
                 end
                 4'b0010: begin  // ADD_CIN
-                    RES = OPA + OPB + CIN;
-                    COUT = RES[8];
+                    {COUT,RES[7:0]} = OPA + OPB + CIN;
                 end
                 4'b0011: begin  // SUB_CIN
                     OFLOW = (OPA < OPB);
@@ -47,29 +56,29 @@ module alu_reference_model(
                 4'b1000: begin  // CMP
                     RES = 9'bz;
                     if (OPA == OPB) begin
-                        E = 1'b1; G = 1'bz; L = 1'bz;
+                        E = 1'b1; G = 1'b0; L = 1'b0;
                     end else if (OPA > OPB) begin
-                        E = 1'bz; G = 1'b1; L = 1'bz;
+                        E = 1'b0; G = 1'b1; L = 1'b0;
                     end else begin
-                        E = 1'bz; G = 1'bz; L = 1'b1;
+                        E = 1'b0; G = 1'b0; L = 1'b1;
                     end
                 end
             endcase
         end 
         else begin  // Logical Mode
             case(CMD)
-                4'b0000: RES = {1'b0, OPA & OPB};       // AND
-                4'b0001: RES = {1'b0, ~(OPA & OPB)};    // NAND
-                4'b0010: RES = {1'b0, OPA | OPB};       // OR
-                4'b0011: RES = {1'b0, ~(OPA | OPB)};    // NOR
-                4'b0100: RES = {1'b0, OPA ^ OPB};       // XOR
-                4'b0101: RES = {1'b0, ~(OPA ^ OPB)};    // XNOR
-                4'b0110: RES = {1'b0, ~OPA};            // NOT_A
-                4'b0111: RES = {1'b0, ~OPB};            // NOT_B
-                4'b1000: RES = {1'b0, OPA >> 1};        // SHR1_A
-                4'b1001: RES = {1'b0, OPA << 1};        // SHL1_A
-                4'b1010: RES = {1'b0, OPB >> 1};        // SHR1_B
-                4'b1011: RES = {1'b0, OPB << 1};        // SHL1_B
+                4'b0000: RES = {8'b0, OPA & OPB};       // AND
+                4'b0001: RES = {8'b0, ~(OPA & OPB)};    // NAND
+                4'b0010: RES = {8'b0, OPA | OPB};       // OR
+                4'b0011: RES = {8'b0, ~(OPA | OPB)};    // NOR
+                4'b0100: RES = {8'b0, OPA ^ OPB};       // XOR
+                4'b0101: RES = {8'b0, ~(OPA ^ OPB)};    // XNOR
+                4'b0110: RES = {8'b0, ~OPA};            // NOT_A
+                4'b0111: RES = {8'b0, ~OPB};            // NOT_B
+                4'b1000: RES = {8'b0, OPA >> 1};        // SHR1_A
+                4'b1001: RES = {8'b0, OPA << 1};        // SHL1_A
+                4'b1010: RES = {8'b0, OPB >> 1};        // SHR1_B
+                4'b1011: RES = {8'b0, OPB << 1};        // SHL1_B
                 4'b1100: begin  // ROL_A_B
                     OPA_1 = OPB[0] ? {OPA[6:0], OPA[7]} : OPA;
                     OPB_1 = OPB[1] ? {OPA_1[5:0], OPA_1[7:6]} : OPA_1;
@@ -85,5 +94,6 @@ module alu_reference_model(
             endcase
         end
     end
+end
 
 endmodule
