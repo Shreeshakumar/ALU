6c3b0a48568bbb6e5a3636607d7c1e6ca115423d
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-10 11:21:10 +0530

    Update alu_reference_model.v

--- src/verification/alu_reference_model.v
+++ src/verification/alu_reference_model.v
@@ -77,6 +77,7 @@ module alu_reference_model(
 										RES = s_sub;
 										OFLOW = ( (OPA[7] != OPB[7]) && (s_sub[7] != OPA[7]) );
 									end// msb opa ~= msb opb msb opa is not equal msb res then oflow high
+					default	:	begin	RES = 0;	ERR = 1;	end
 					endcase	
 				end
 			else if (INP_VALID == 2'b01 || INP_VALID == 2'b11)	//only a or ab valid
@@ -84,6 +85,7 @@ module alu_reference_model(
 				case(CMD)
 					4'b0100: 	RES = OPA + 1;  // INC_A
                 	4'b0101: 	RES = OPA - 1;  // DEC_A
+					default	:	begin	RES = 0;	ERR = 1;	end
 				endcase 
 				end
 			else if (INP_VALID == 2'b10 || INP_VALID == 2'b11)	//only b or ab valid
@@ -91,6 +93,7 @@ module alu_reference_model(
 				case(CMD)
 					4'b0110: 	RES = OPB + 1;  // INC_B
 					4'b0111: 	RES = OPB - 1;  // DEC_B
+					default	:	begin	RES = 0;	ERR = 1;	end
 				endcase 
 				end
 			else begin	RES = 0;	ERR = 1;	end
@@ -119,6 +122,7 @@ module alu_reference_model(
 									for( i = 0; i < 8; i = i + 1)
         								RES[i] = OPA[(i + OPB[2:0]) % 8];
                 				end
+						default	:	begin	RES = 0;	ERR = 1;	end
 					endcase 
 				end
 			else if (INP_VALID == 2'b01 || INP_VALID == 2'b11)	//only a or ab valid
@@ -127,6 +131,7 @@ module alu_reference_model(
 						4'b0110: RES[7:0] = ~OPA;            // NOT_A
 						4'b1000: RES[7:0] = OPA >> 1;        // SHR1_A
 						4'b1001: RES[7:0] = OPA << 1;        // SHL1_A
+						default	:	begin	RES = 0;	ERR = 1;	end
 					endcase 
 				end
 			else if (INP_VALID == 2'b10 || INP_VALID == 2'b11)	//only b or ab valid
@@ -135,6 +140,7 @@ module alu_reference_model(
 						4'b0111: RES = {8'b0, ~OPB};            // NOT_B
                 		4'b1010: RES = {8'b0, OPB >> 1};        // SHR1_B
                 		4'b1011: RES = {8'b0, OPB << 1};        // SHL1_B
+						default	:	begin	RES = 0;	ERR = 1;	end
 					endcase 
 				end
 			else begin	RES = 0;	ERR = 1;	end
