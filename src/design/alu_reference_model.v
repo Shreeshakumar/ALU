7988d039ef24ac6d6228bddcfec5ffe4d146acda
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-15 15:57:25 +0530

    Remove CMP logic from ALU reference model
    Removed CMP logic for comparison operations in ALU.

--- src/design/alu_reference_model.v
+++ src/design/alu_reference_model.v
@@ -70,27 +70,11 @@ module alu_reference_model(
 					4'd11	:	begin	//A n B signed A+B
 										RES = s_add;
 										OFLOW = ( (OPA[7] == OPB[7]) && (s_add[7] != OPA[7]) );
-
-								begin  // CMP                    			
-								if (OPA == OPB) begin	E = 1'b1; G = 1'b0; L = 1'b0;	end 
-								else if (OPA > OPB) begin	E = 1'b0; G = 1'b1; L = 1'b0;end 
-								else begin	E = 1'b0; G = 1'b0; L = 1'b1;	end
-								//else begin	E = 1'b0; G = 1'b0; L = 1'b0;	end
-                				end
-
 									end// msb opa = msb opb msb opa is not equal msb res then oflow high
 						
 					4'd12	:	begin	//A n B signed A-B
 										RES = s_sub;
 										OFLOW = ( (OPA[7] != OPB[7]) && (s_sub[7] != OPA[7]) );
-
-								begin  // CMP                    			
-								if (OPA == OPB) begin	E = 1'b1; G = 1'b0; L = 1'b0;	end 
-								else if (OPA > OPB) begin	E = 1'b0; G = 1'b1; L = 1'b0;end 
-								else begin	E = 1'b0; G = 1'b0; L = 1'b1;	end
-								//else begin	E = 1'b0; G = 1'b0; L = 1'b0;	end
-                				end
-						
 									end// msb opa ~= msb opb msb opa is not equal msb res then oflow high
 					default	:	begin	RES = 0;	ERR = 1;	end
 					endcase	
